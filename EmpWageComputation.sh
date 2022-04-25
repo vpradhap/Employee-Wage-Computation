4f958266ac280e385ef5087e2ef59fceec760505
vpradhap <pradhapvenkat@gmail.com>
2022-04-25 20:30:51 +0530

    modified code to Switch case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -7,18 +7,24 @@ PART_TIME=2
 
 present=$((RANDOM%3))
 
-if [ $present -eq $PRESENT ]
-then
-	dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR))
-	echo "employee is present"
-elif [ $present -eq $PART_TIME ]
-then
-	dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR/2))
-	echo "employee is part timer"
-else
-	dailywage=0
-	echo "employee is absent"
-fi
+case $present in
+
+	$PRESENT)
+		dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR))
+		echo "employee is present"
+	;;
+
+	$PART_TIME)
+		dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR/2))
+		echo "employee is part timer"
+	;;
+
+	*)
+		dailywage=0
+		echo "employee is absent"
+	;;
+
+esac
 
 echo "employee daily wage : $"$dailywage "USD"
 
