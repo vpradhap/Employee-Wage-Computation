993ecd606873742abddd39f033d550ae9c9d845c
vpradhap <pradhapvenkat@gmail.com>
2022-04-25 20:45:34 +0530

    changed code to find monthly wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,27 +4,31 @@ EMP_WAGE_PER_HOUR=20
 PRESENT=1
 WORKING_HOUR=8
 PART_TIME=2
+MAX_WORKING_DAY=20
 
-present=$((RANDOM%3))
+totalsalary=0
 
-case $present in
+for (( day=0;day<$MAX_WORKING_DAY; day++ ))
+do
+	present=$((RANDOM%3))
 
-	$PRESENT)
-		dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR))
-		echo "employee is present"
-	;;
+	case $present in
 
-	$PART_TIME)
-		dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR/2))
-		echo "employee is part timer"
-	;;
+		$PRESENT)
+			dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR))
+		;;
 
-	*)
-		dailywage=0
-		echo "employee is absent"
-	;;
+		$PART_TIME)
+			dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR/2))
+		;;
 
-esac
+		*)
+			dailywage=0
+		;;
+	esac
 
-echo "employee daily wage : $"$dailywage "USD"
+	totalsalary=$((totalsalary + dailywage))
+done
+
+echo "employee monthly wage : $"$totalsalary "USD"
 
