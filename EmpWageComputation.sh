da9751aaf83b06ee3df6136cf6940f329b74b056
vpradhap <pradhapvenkat@gmail.com>
2022-04-25 20:09:23 +0530

    Daily Wage is Added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+EMP_WAGE_PER_HOUR=20
+PRESENT=1
+WORKING_HOUR=8
+
 present=$((RANDOM%2))
 
-if [ $present -eq 1 ]
+if [ $present -eq $PRESENT ]
 then
+	dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR))
 	echo "employee is present"
 else
+	dailywage=0
 	echo "employee is absent"
 fi
+
+echo "employee daily wage : $"$dailywage "USD"
 
