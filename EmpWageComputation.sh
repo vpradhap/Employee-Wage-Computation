21453800a7a2ececab0c10ac9258284a35d48f18
vpradhap <pradhapvenkat@gmail.com>
2022-04-25 20:24:50 +0530

    parttime employee wage also added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -3,13 +3,18 @@
 EMP_WAGE_PER_HOUR=20
 PRESENT=1
 WORKING_HOUR=8
+PART_TIME=2
 
-present=$((RANDOM%2))
+present=$((RANDOM%3))
 
 if [ $present -eq $PRESENT ]
 then
 	dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR))
 	echo "employee is present"
+elif [ $present -eq $PART_TIME ]
+then
+	dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR/2))
+	echo "employee is part timer"
 else
 	dailywage=0
 	echo "employee is absent"
